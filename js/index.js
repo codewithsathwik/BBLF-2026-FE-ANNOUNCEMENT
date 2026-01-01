dd94fbc7d57e18fb054aca0e7940f33be305222e
codewithsathwik <sathvikms344@gmail.com>
2026-01-01 23:06:43 +0530

    first commit

---
+++ js/index.js
@@ -0,0 +1,102 @@
+
+// ================sliders================
+
+const playCards = document.querySelectorAll(".play-card");
+const nextBtn = document.querySelector(".next");
+const prevBtn = document.querySelector(".prev");
+const playlistFrame = document.querySelector(".video-iframe iframe");
+const allCards = document.querySelector(".sliders");
+
+
+let firstCard;
+let totalWidth;
+let visibleWidth;
+let maxOffset;
+
+
+
+let offset = 0;
+
+const defaultSize = 250; // in px 
+
+playCards.forEach(async (card, i) => {
+    let link = card.getAttribute("data-target");
+    getYouTubeTitle(link, card, i);
+    card.addEventListener("click", () => {
+        playCards.forEach(crd => crd.classList.remove("active-slide"));
+        card.classList.add("active-slide");
+        let title = card.querySelector("img").alt;
+        changePlaylistSrc(link, title);
+    });
+});
+
+
+function changePlaylistSrc(link, title) {
+    playlistFrame.setAttribute("src", link);
+    playlistFrame.setAttribute("title", title);
+}
+
+function setThumbAlt(thumb, card, alt, i) {
+    let img = card.querySelector("img");
+    img.src = thumb;
+    img.setAttribute("alt", alt);
+    if (i === 0) {
+        let link = card.getAttribute("data-target");
+        changePlaylistSrc(link, alt)
+    }
+}
+
+function applyTransform() {
+    playCards.forEach(card => {
+        card.style.transform = `translateX(-${offset}px)`;
+    });
+}
+
+nextBtn.addEventListener("click", () => {
+    calculateOffsetWidth();
+    offset = Math.min(offset + defaultSize, maxOffset);
+    applyTransform();
+});
+
+prevBtn.addEventListener("click", () => {
+    calculateOffsetWidth();
+    offset = Math.max(offset - defaultSize, 0);
+    applyTransform();
+});
+
+function calculateOffsetWidth() {
+    firstCard = playCards[0];
+    totalWidth = firstCard.parentElement.scrollWidth;
+    visibleWidth = firstCard.parentElement.clientWidth;
+    maxOffset = totalWidth - visibleWidth;
+}
+
+
+function getYouTubeTitle(link, card, i) {
+    const videoId = link.split('/embed/')[1].split('?')[0];
+    const oembedUrl = `https://www.youtube.com/oembed?url=https://www.youtube.com/watch?v=${videoId}`;
+    const thumb = `https://img.youtube.com/vi/${videoId}/maxresdefault.jpg`;
+
+    fetch(oembedUrl)
+        .then(response => response.json())
+        .then(data => {
+            setThumbAlt(thumb, card, data.title, i);
+        })
+        .catch(error => console.error('Error:', error));
+}
+
+
+//add number to terms and condition
+const numSpan = document.querySelectorAll(".term-num");
+const conditions = document.querySelectorAll(".condition");
+let conditionNum = 0;
+
+if (conditions) {
+    conditions.forEach((condition) => {
+        let numSpan = condition.querySelectorAll(".term-num");
+        conditionNum += 1;
+        numSpan.forEach((numSpan) => {
+            numSpan.textContent = conditionNum;
+        });
+    });
+}
